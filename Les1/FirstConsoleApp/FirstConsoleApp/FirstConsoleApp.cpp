4cde500c9097a55352f1ca068b708f6c9ec70ae6
gijsoman <gijsbakker1@hotmail.com>
2017-09-10 10:40:31 +0200

    My first c++ program (lies)
    My first c++ program (lies)

---
+++ Les1/FirstConsoleApp/FirstConsoleApp/FirstConsoleApp.cpp
@@ -0,0 +1,16 @@
+// FirstConsoleApp.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+int main()
+{
+	std::cout << "Hello, World!" << std::endl;
+	
+	char c;
+	c = std::cin.get();
+
+	return 0;
+}
+
---
+++ Les1/FirstConsoleApp/FirstConsoleApp/Myfirstclass.h
@@ -0,0 +1,8 @@
+#pragma once
+class Myfirstclass
+{
+public:
+	Myfirstclass();
+	~Myfirstclass();
+};
+
